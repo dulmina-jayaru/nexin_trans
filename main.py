ee510f0cc47b59ab06ab309319840ae5b8bb4f9c
Adolf Sipes <112796306+sltechnolk@users.noreply.github.com>
2023-06-12 15:45:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from googletrans import Translator
+
+app = Flask(__name__)
+
+# Read API keys from apis.txt file
+def read_api_keys():
+    api_keys = []
+    with open("apis.txt", "r") as file:
+        for line in file:
+            api_keys.append(line.strip())
+    return api_keys
+
+# Check if the provided API key is valid
+def is_valid_api_key(api_key):
+    api_keys = read_api_keys()
+    return api_key in api_keys
+
+# API endpoint for processing requests
+@app.route("/translate/<api_key>/<text>/<target_language>")
+def translate_text(api_key, text, target_language):
+    # Check if the API key is valid
+    if not is_valid_api_key(api_key):
+        return "Invalid API key"
+
+    # Perform the translation
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language).text
+
+    return translation
+
+if __name__ == "__main__":
+    app.run()
